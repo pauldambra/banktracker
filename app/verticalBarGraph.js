af47d268c8387956e88b9b3888993aaceaf85493
pauldambra <pauldambra@gmail.com>
2016-10-17 23:43:02 +0100

    add a first version of a spending type graph

--- app/verticalBarGraph.js
+++ app/verticalBarGraph.js
@@ -1,13 +1,65 @@
 'use strict';
 
 import React from 'react';
-import { toPairs } from 'lodash';
+import { toPairs, max, min, rangeRight } from 'lodash';
 
-const Bar = props => (
-  <div className={`bar ${props.seriesKey}`} style={{height: props.height + 'px'}}>
+const containerHeight = 250;
+
+const calculateBarOffset = (barValue, max, min) => {
+  const range = max - min;
+  const diff = max - barValue;
+  const pixelsPerUnit = containerHeight/range;
+  return diff*pixelsPerUnit;
+};
+
+const CenterAxis = props => {
+  const top = calculateBarOffset(0, props.max, props.min);
+  return <div className="center-axis" style={{top: top + 'px'}}></div>
+};
+
+const Bar = props => {
+  const valOffset = calculateBarOffset(props.value, props.max, props.min);
+  const zeroOffset = calculateBarOffset(0, props.max, props.min);
+  
+  const height = Math.abs(valOffset - zeroOffset);
+  const top = Math.min(valOffset, zeroOffset);
+
+  return <div className={`bar ${props.seriesKey.replace(' ', '-')}`} 
+       style={{top: top + 'px', height: height + 'px'}}>
     {props.seriesKey}
   </div>
-);
+};
+
+const createRange = (min, max) => {
+  if (!min || !max) {return [];}
+
+  if (max < 0) { max = 0; }
+
+  const range = rangeRight(min, max, 10);
+  range.unshift(max);
+  return range;
+}
+
+class VerticalAxis extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {range: props.range || []};
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState(nextProps);
+  }
+
+  render() {
+    const points = this.state.range.map(r => {
+      return <div key={r} className="point">{r}</div>;      
+    })
+
+    return <div className="left-axis">
+            {points}
+          </div>
+  }
+}
 
 export default class VerticalBarGraph extends React.Component {
   constructor(props) {
@@ -21,10 +73,23 @@ export default class VerticalBarGraph extends React.Component {
   }
 
   render() {
-    const series = toPairs(this.state)
+    const pairs = toPairs(this.state)
+    const values = pairs.map(p=>p[1]);
+    const range = createRange(min(values), max(values), 10);
+    const series = pairs
                     .map((el, i) => {
-                      return <Bar key={i} seriesKey={el[0]} height={el[1]}/>;
+                      return <Bar key={i} 
+                                  seriesKey={el[0]} 
+                                  value={el[1]} 
+                                  max={max(values)}
+                                  min={min(values)}/>;
                     });
-    return <div>{series}</div>;
+    return <div>
+            <CenterAxis min={min(values)} max={max(values)}/>
+            <div id="graph">
+              <VerticalAxis range={range}/>
+              {series}
+            </div>
+          </div>
   }
 }
--- test/verticalBar.spec.js
+++ test/verticalBar.spec.js
@@ -4,15 +4,73 @@ import VerticalBarGraph from '../app/verticalBarGraph';
 import Rx from 'rx';
 
 describe('the vertical bar graph', function() {
-    it('can show data', function() {
-        const subject = new Rx.Subject();
+  const expectedData = {
+    switch: -4, 
+    dd: -10, 
+    cash: -20, 
+    'child benefit': 30
+  };
+  let subject;
+  let wrapper;
 
-        const wrapper = mount(<VerticalBarGraph Data={subject} />);
-        
-        subject.onNext({switch: -4, dd: -10, cash: -20, 'child benefit': 30});
-        const switchBar = wrapper.find('.bar.switch');
+  beforeEach(function() {
+    subject = new Rx.Subject();
+    wrapper = mount(<VerticalBarGraph Data={subject} />);
+  });
 
-        const expectedHtml = '<div class="bar switch" style="height: -4px;">switch</div>';
-        switchBar.html().should.be.exactly(expectedHtml);
-    });
+  it('can show data', function() {
+    subject.onNext(expectedData);
+    const switchBar = wrapper.find('.bar.switch');
+    const expectedHtml = '<div class="bar switch" style="top: 150px; height: 20px;">switch</div>';
+    switchBar.html().should.be.exactly(expectedHtml);
+  });
+
+  it('can calculate left axis', function() {
+    subject.onNext(expectedData);
+    const points = wrapper.find('.left-axis .point')
+                          .map(p => p.html())
+                          .map(p => p.replace('<div class="point">', ''))
+                          .map(p => p.replace('</div>', ''));
+    points[0].should.be.exactly('30');
+    points[points.length-1].should.be.exactly('-20');
+  });
+
+  it('can calculate proportion of height to 0 for center axis', function() {
+    subject.onNext(expectedData);
+    const center = wrapper.find('.center-axis');
+    center.html().should.containEql('top: 150px');
+  });
+
+  it('can make sure the left axis always includes 0', function() {
+    subject.onNext({a: -4, b: -10, c: -40});
+    const points = wrapper.find('.left-axis .point')
+                      .map(p => p.html())
+                      .map(p => p.replace('<div class="point">', ''))
+                      .map(p => p.replace('</div>', ''));
+    points[0].should.be.exactly('0');
+  });
+
+  it('can set top for a positive bar', function() {
+    subject.onNext(expectedData);
+    const bar = wrapper.find('.bar.child-benefit');
+    bar.html().should.containEql('top: 0px');
+  });
+
+  it('can set height for a positive bar', function() {
+    subject.onNext(expectedData);
+    const bar = wrapper.find('.bar.child-benefit');
+    bar.html().should.containEql('height: 150px');
+  });
+
+  it('can set top for a negative bar', function() {
+    subject.onNext(expectedData);
+    const bar = wrapper.find('.bar.dd');
+    bar.html().should.containEql('top: 150px');
+  });
+
+  it('can set height for a positive bar', function() {
+    subject.onNext(expectedData);
+    const bar = wrapper.find('.bar.dd');
+    bar.html().should.containEql('height: 50px');
+  });
 });
